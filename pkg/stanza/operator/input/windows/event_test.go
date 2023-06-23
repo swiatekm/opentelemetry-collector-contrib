56f1acf32bb24f9772ba47bf16ba5c6a9c76163c
Mikolaj Swiatek <mswiatek@sumologic.com>
2023-06-23 11:40:00 +0200

    Fix buffer overflow when reading raw events

--- pkg/stanza/operator/input/windows/event_test.go
+++ pkg/stanza/operator/input/windows/event_test.go
@@ -7,7 +7,12 @@
 package windows
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+	"unsafe"
+
+	"golang.org/x/text/encoding/unicode"
 
 	"github.com/stretchr/testify/require"
 )
@@ -32,4 +37,65 @@ func TestEventCloseSuccess(t *testing.T) {
 	err := event.Close()
 	require.NoError(t, err)
 	require.Equal(t, uintptr(0), event.handle)
+}
+
+func TestEventRenderSimple(t *testing.T) {
+	data, err := os.ReadFile(filepath.Join("testdata", "xmlSample.xml"))
+	dataUTF16, err := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewEncoder().Bytes(data)
+	require.NoError(t, err)
+	event := NewEvent(5)
+	buf := NewBuffer()
+	renderProc = renderMockProc(dataUTF16)
+	_, err = event.RenderSimple(buf)
+	require.NoError(t, err)
+}
+
+// same test as above, but with an initial buffer smaller than the event data
+func TestEventRenderSimpleSmallBuffer(t *testing.T) {
+	data, err := os.ReadFile(filepath.Join("testdata", "xmlSample.xml"))
+	dataUTF16, err := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewEncoder().Bytes(data)
+	require.NoError(t, err)
+	event := NewEvent(5)
+	buf := Buffer{buffer: make([]byte, len(dataUTF16)/2)}
+	renderProc = renderMockProc(dataUTF16)
+	_, err = event.RenderSimple(buf)
+	require.NoError(t, err)
+}
+
+func TestEventRenderRaw(t *testing.T) {
+	data, err := os.ReadFile(filepath.Join("testdata", "xmlSample.xml"))
+	dataUTF16, err := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewEncoder().Bytes(data)
+	require.NoError(t, err)
+	event := NewEvent(5)
+	buf := NewBuffer()
+	renderProc = renderMockProc(dataUTF16)
+	_, err = event.RenderRaw(buf)
+	require.NoError(t, err)
+}
+
+// same test as above, but with an initial buffer smaller than the event data
+func TestEventRenderRawSmallBuffer(t *testing.T) {
+	data, err := os.ReadFile(filepath.Join("testdata", "xmlSample.xml"))
+	dataUTF16, err := unicode.UTF16(unicode.LittleEndian, unicode.UseBOM).NewEncoder().Bytes(data)
+	require.NoError(t, err)
+	event := NewEvent(5)
+	buf := Buffer{buffer: make([]byte, len(dataUTF16)/2)}
+	renderProc = renderMockProc(dataUTF16)
+	_, err = event.RenderRaw(buf)
+	require.NoError(t, err)
+}
+
+func renderMockProc(data []byte) MockProc {
+	return MockProc{
+		call: func(args ...uintptr) (uintptr, uintptr, error) {
+			bufferSize, buffer, bufferUsed := (uint32)(args[3]), unsafe.Pointer(args[4]), unsafe.Pointer(args[5])
+			bufferSlice := unsafe.Slice((*byte)(buffer), bufferSize)
+			*(*uint32)(bufferUsed) = uint32(len(data))
+			if int(bufferSize) < len(data) {
+				return 0, 0, ErrorInsufficientBuffer
+			}
+			copy(bufferSlice, data)
+			return 0, 0, nil
+		},
+	}
 }
